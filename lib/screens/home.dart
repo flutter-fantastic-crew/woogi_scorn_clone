784d708784cf064442a9d153796c758a9e697178
woogi <dbwhddnr00@naver.com>
2024-02-18 23:16:50 +0900

    앱바 추가

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,8 +12,34 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: BadgeWidget(
-      badgeText: "요약",
-    ));
+        appBar: AppBar(
+          title: Row(children: [
+            const Padding(
+              padding: EdgeInsets.only(right: 11),
+              child: Text("내 플랜",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            ),
+            BadgeWidget(
+              badgeText: "요약",
+              backgroundColor: Colors.grey,
+              textColor: Colors.white,
+            )
+          ]),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.add,
+                  color: Colors.grey,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.more_horiz,
+                  color: Colors.grey,
+                ))
+          ],
+        ),
+        body: Container());
   }
 }
--- lib/widget/badge_widget.dart
+++ lib/widget/badge_widget.dart
@@ -2,25 +2,26 @@ import 'package:flutter/material.dart';
 
 class BadgeWidget extends StatelessWidget {
   BadgeWidget(
-      {Key? key, required this.badgeText, this.backgroundColor, this.textColor})
+      {Key? key,
+      required this.badgeText,
+      required this.backgroundColor,
+      required this.textColor})
       : super(key: key);
   String badgeText;
-  Color? backgroundColor = Colors.grey;
-  Color? textColor = Colors.white;
+  Color backgroundColor;
+  Color textColor;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(40), color: backgroundColor),
-        height: 66,
-        child: Padding(
-          padding: const EdgeInsets.only(left: 15, right: 15, top: 3),
-          child: Text(
-            badgeText,
-            style: TextStyle(fontSize: 40, color: textColor),
-          ),
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(40), color: backgroundColor),
+      width: 28,
+      height: 17,
+      child: Center(
+        child: Text(
+          badgeText,
+          style: TextStyle(fontSize: 10, color: textColor),
         ),
       ),
     );
