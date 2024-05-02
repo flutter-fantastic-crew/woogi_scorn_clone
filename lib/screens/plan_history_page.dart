edd0658b744245b6c4d1c657a1be0427dcb41ee4
woogi <dbwhddnr00@naver.com>
2024-05-02 22:29:09 +0900

    내역 추가 UI 추가

--- lib/screens/plan_history_page.dart
+++ lib/screens/plan_history_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:scorn_clone/widget/row_text_field.dart';
 
+import '../view_model/home_page_view_model.dart';
 import '../widget/custom_appbar.dart';
 
 class PlanHistoryPage extends StatelessWidget {
@@ -15,34 +16,89 @@ class PlanHistoryPage extends StatelessWidget {
         backGroundColor: Colors.white,
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            children: [
-              RowTextField(widgetList: [
-                IconButton(
-                  icon: Icon(Icons.add),
-                  onPressed: () {},
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                RowTextField(widgetList: [
+                  IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () {},
+                  ),
+                  const Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "소비 금액",
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 30,
+                    child: Text("원",
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontFamily: 'PretendardRegular',
+                            color: Colors.grey[400])),
+                  ),
+                ]),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  child: RowTextField(widgetList: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20, right: 10),
+                      child: GestureDetector(
+                        child: Text(getEmoji),
+                      ),
+                    ),
+                    const Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "내용",
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ]),
                 ),
-                Text("소비 금액"),
-                Text("원"),
-              ]),
-            ],
-          ),
-          ElevatedButton(
-            onPressed: null,
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blueAccent,
-              foregroundColor: Colors.white,
-              minimumSize: Size(700, 50),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12)),
+                RowTextField(widgetList: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15, right: 10),
+                    child: Icon(Icons.calendar_month),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: DateTime.now().toString(),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ]),
+              ],
             ),
-            child: const Text("저장",
-                style: TextStyle(fontSize: 18, fontFamily: 'PretendardBold')),
-          ),
-        ],
+            ElevatedButton(
+              onPressed: null,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blueAccent,
+                foregroundColor: Colors.white,
+                disabledBackgroundColor: Colors.blueAccent[100],
+                minimumSize: Size(700, 50),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12)),
+              ),
+              child: const Text("저장",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'PretendardBold',
+                    color: Colors.white,
+                  )),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/widget/row_text_field.dart
+++ lib/widget/row_text_field.dart
@@ -9,9 +9,19 @@ class RowTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [...widgetList.map((widget) => widget)],
+    return Container(
+      height: 60,
+      decoration: BoxDecoration(
+        color: Colors.grey[100],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(right: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [...widgetList.map((widget) => widget)],
+        ),
+      ),
     );
   }
 }
